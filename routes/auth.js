c2f179e3c83565df718d7f2e183b00e110b01dfa
Salih041 <salihozbk41@gmail.com>
2025-11-23 15:45:04 +0300

    Add Email; input validation (register,login); Upgrade security , helmet,rate-limit

--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,76 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { body, validationResult } from "express-validator";
 import User from "../models/User.js";
 
 const router = express.Router();
 
-router.post("/register", async (req,res)=>{
-    try{
-        const {username, password} = req.body;
+router.post("/register", [
+    body("username").trim().notEmpty().withMessage("Username is required").isLength({ min: 3 }).withMessage("Username must be at least 3 characters"),
+    body("email").trim().isEmail().withMessage("Invalid Email").normalizeEmail(),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters").matches(/\d/).withMessage("Password must contain at least one number")
+],
+    async (req, res) => {
+        try {
 
-        const existingUser = await User.findOne({ username: username });
-        if (existingUser) {
-            return res.status(400).json({ message: "Bu kullanıcı adı zaten alınmış." });
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: errors.array()[0].msg });
+            }
+
+            const { username, email, password } = req.body;
+
+            const existingUser = await User.findOne({ $or: [{ username: username }, { email: email }] });
+            if (existingUser) {
+                return res.status(400).json({ message: "This username or email is already taken" });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                username,
+                email,
+                password: hashedPassword
+            })
+
+            const savedUser = await newUser.save();
+            res.status(201).json({ message: "Kullanıcı oluşturuldu", userId: savedUser._id });
+        } catch (error) {
+            if (error.code === 11100) return res.status(400).json({ message: "There is already a user with this informations." });
+            res.status(500).json({ error: error.message });
         }
-        
-        const hashedPassword = await bcrypt.hash(password,12);
+    })
 
-        const newUser = new User({
-            username,
-            password : hashedPassword
-        })
+router.post("/login",
+    [
+        body("username").notEmpty().withMessage("Username is required"),
+        body("password").notEmpty().withMessage("Password is required")
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: errors.array()[0].msg });
+            }
 
-        const savedUser = await newUser.save();
-        res.status(201).json({message : "Kullanıcı oluşturuldu", userId:savedUser._id});
-    }catch(error){
-        res.status(500).json({error : error.message});
-    }
-})
+            const { username, password } = req.body;
+            const user = await User.findOne({ username });
+            if (!user) return res.status(404).json({ message: "User not found" });
 
-router.post("/login", async (req,res)=>{
-    try{
-        const {username, password} = req.body;
-        const user = await User.findOne({username});
-        if(!user) return res.status(404).json({message : "Kullanıcı Bulunamadı"});
+            const isPasswordCorrect = await bcrypt.compare(password, user.password);
+            if (!isPasswordCorrect) return res.status(400).json({ message: "Wrong Password" });
 
-        const isPasswordCorrect = await bcrypt.compare(password,user.password);
-        if(!isPasswordCorrect) return res.status(400).json({message : "Şifre yanlış"});
+            const token = jwt.sign(
+                { userID: user._id, username: user.username },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h" }
+            )
 
-        const token = jwt.sign(
-            {userID : user._id, username : user.username},
-            process.env.JWT_SECRET,
-            {expiresIn : "1h"}
-        )
-
-        res.status(200).json({token,userID:user._id});
-    }catch(error){
-        res.status(500).json({error : error.message});
-    }
-})
+            res.status(200).json({ token, userID: user._id });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    })
 
 export default router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import authRoutes from "./routes/auth.js"
 import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/users.js"
@@ -11,20 +13,29 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const dburl = process.env.MONGO_URL;
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15min
+    max: 50,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Too many request."
+});
+app.use(limiter())
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(dburl)
-    .then(()=>{
+    .then(() => {
         console.log("MongoDB connected");
-        app.listen(PORT,()=>{console.log("Server is running on " , PORT)});
-    }).catch((err)=>{
+        app.listen(PORT, () => { console.log("Server is running on ", PORT) });
+    }).catch((err) => {
         console.error("MongoDB error");
         console.error(err);
         process.exit(1);
     })
 
-app.use("/api/auth",authRoutes);
-app.use("/api/posts",postRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
 
