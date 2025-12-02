8f91517b53f02ecbf2f410ce58b7b2c056cb34dd
Salih041 <salihozbk41@gmail.com>
2025-12-02 16:41:02 +0300

    Prepare Resend

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,26 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import { Resend } from "resend";
 
 dotenv.config();
+
+/*const resend = new Resend(process.env.RESEND_KEY);
+const sendEmail = async (options) => {
+    try {
+        const data = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: options.email,
+            subject: options.subject,
+            html: options.html
+        })
+        console.log("✅ Email sent (Resend ID):", data.id);
+        return data;
+    } catch (error) {
+        console.error("❌ Resend Error:", error);
+        throw new Error("Email couldnt be sent: " + error.message);
+    }
+}*/
+
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
