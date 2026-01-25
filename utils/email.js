442f5183f24b910b47039a300a5d35615a7d77dd
Salih041 <salihozbk41@gmail.com>
2026-01-25 19:17:09 +0300

    cleaning

--- utils/email.js
+++ utils/email.js
@@ -21,29 +21,4 @@ const sendEmail = async (options) => {
     }
 }
 
-/*
-const sendEmail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, 
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    const mailOptions = {
-        from: `"SelamY Blog" <${process.env.EMAIL_USER}>`,
-        to: options.email,
-        subject: options.subject,
-        html: options.html
-    };
-
-    await transporter.sendMail(mailOptions);
-}
-*/
 export default sendEmail;
